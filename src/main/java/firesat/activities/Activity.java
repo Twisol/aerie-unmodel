4a51efa77c94e67b07c470295fefd7baac6ef555
Jonathan Castello <twisolar@gmail.com>
2023-08-28 23:15:00 -0700

    Implement a catch-all activity with properties and a fixed duration

---
+++ src/main/java/firesat/activities/Activity.java
@@ -0,0 +1,20 @@
+package firesat.activities;
+
+import firesat.Mission;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType.ControllableDuration;
+import gov.nasa.jpl.aerie.merlin.framework.annotations.ActivityType.EffectModel;
+import gov.nasa.jpl.aerie.merlin.protocol.types.Duration;
+
+import java.util.Map;
+
+import static gov.nasa.jpl.aerie.merlin.framework.ModelActions.delay;
+
+@ActivityType("Span")
+public record Activity(Duration duration, Map<String, String> properties) {
+  @EffectModel
+  @ControllableDuration(parameterName = "duration")
+  public void run(Mission mission) {
+    if (this.duration.isPositive()) delay(this.duration);
+  }
+}
